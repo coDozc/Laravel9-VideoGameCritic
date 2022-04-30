d69e28e011135cf376a8ac85ac7472fcd8a5ff4b
coDozc <ozcan_bayram57@hotmail.com>
2022-05-01 01:16:02 +0300

    show ve delete ayarlandı

---
+++ resources/views/admin/category/show.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.adminbase')
+
+@section('title', 'Show Category: {{$data->title}}')
+
+
+@section('content')
+    <link href="assets/css/bootstrap-fileupload.min.css" rel="stylesheet" />
+    <div id="page-wrapper">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                Edit Category: {{$data->title}}
+            </div>
+            <div class="col-md-6">
+                <!--    Context Classes  -->
+                <div class="panel panel-default">
+                    <a href="/admin/category/edit/{{$data->id}}" class="btn btn-info" style="margin: 20px 15px 30px 30px ">Edit</a>
+                    <a href="/admin/category/destroy/{{$data->id}}" class="btn btn-info" style="margin: 20px 15px 30px 30px ">Delete</a>
+                    <div class="panel-heading">
+                        Details
+                    </div>
+
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table">
+
+                                <tr>
+                                    <th style="width: 150px" >Id</th>
+                                    <td>{{$data->id}}</td>
+                                </tr>
+                                <tr>
+                                    <th style="width: 30px" >Title</th>
+                                    <td>{{$data->title}}</td>
+                                </tr>
+                                <tr>
+                                    <th style="width: 30px" >Keywords</th>
+                                    <td>{{$data->keywords}}</td>
+                                </tr>
+                                <tr>
+                                    <th style="width: 30px" >Image</th>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <th style="width: 30px" >Status</th>
+                                    <td>{{$data->status}}</td>
+                                </tr>
+                                <tr>
+                                    <th style="width: 30px" >Created Date</th>
+                                    <td>{{$data->created_at}}</td>
+                                </tr>
+                                <tr>
+                                    <th style="width: 30px" >Updated Date</th>
+                                    <td>{{$data->updated_at}}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!--  end  Context Classes  -->
+            </div>
+        <!-- /. PAGE INNER  -->
+    </div>
+    <!-- /. PAGE WRAPPER  -->
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,10 @@ Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin');
 Route::get('/admin/category', [CategoryController::class, 'index'])->name('admin_category');
 Route::get('/admin/category/create', [CategoryController::class, 'create'])->name('admin_category_create');
 Route::put('/admin/category/store', [CategoryController::class, 'store'])->name('admin_category_store');
-Route::get('/admin/category/edit{id}', [CategoryController::class, 'edit'])->name('admin_category_edit');
+Route::get('/admin/category/edit/{id}', [CategoryController::class, 'edit'])->name('admin_category_edit');
 Route::put('/admin/category/update/{id}', [CategoryController::class, 'update'])->name('admin_category_update');
+Route::get('/admin/category/destroy/{id}', [CategoryController::class, 'destroy'])->name('admin_category_destroy');
+Route::get('/admin/category/show/{id}', [CategoryController::class, 'show'])->name('admin_category_show');
 
 
 require __DIR__.'/auth.php';
